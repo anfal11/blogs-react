59aa7567f447f385c82e509073ab27413e88c8ab
anfal11 <anfal15-14303@diu.edu.bd>
2023-09-14 02:06:47 +0600

    fake json data created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Blogs />
+    </>
+  )
+}
+
+export default App
